66abc5a467ebb10801a1a9e6421a3567f772c736
Yusuke Urushihata <uruppe@gmail.com>
2015-12-31 11:57:18 +0900

    Added Ranger configurations

--- mklinks.sh
+++ mklinks.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
 ln -sf ~/gitroot/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/gitroot/dotfiles/ranger/rc.conf ~/.config/ranger/
+ln -sf ~/gitroot/dotfiles/ranger/rifle.conf ~/.config/ranger/
+ln -sf ~/gitroot/dotfiles/ranger/scope.sh ~/.config/ranger/
