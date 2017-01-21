a2d69f93c09d403a66688fa444f1c6872a88bba6
Urushihata Yusuke <uruppe@gmail.com>
2017-01-22 07:55:49 +0900

    added vifm dotfile link

--- mklinks.sh
+++ mklinks.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 ln -sf ~/gitroot/dotfiles/.zshrc ~/.zshrc
+
 ln -sf ~/gitroot/dotfiles/ranger/rc.conf ~/.config/ranger/
 ln -sf ~/gitroot/dotfiles/ranger/rifle.conf ~/.config/ranger/
 ln -sf ~/gitroot/dotfiles/ranger/scope.sh ~/.config/ranger/
+
+ln -sf ~/gitroot/dotfiles/vifm/vifmrc ~/.config/vifm/
