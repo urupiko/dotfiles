6472f12c6b31d74df29993e608b1ee45e3d9b175
urupiko <uruppe@gmail.com>
2021-12-31 01:45:48 +0900

    Added VS Code settings

--- macos/mklinks.sh
+++ macos/mklinks.sh
@@ -14,6 +14,10 @@ ln -sf ~/gitroot/dotfiles/peco/config.json ~/.config/peco/
 dir=~/.config/nvim; [ ! -e $dir ] && mkdir -p $dir
 ln -sf ~/gitroot/dotfiles/neovim/init.vim ~/.config/nvim/
 
+dir="${HOME}/Library/Application Support/Code/User"; [ ! -e "${dir}" ] && mkdir -p "${dir}"
+ln -sf ~/gitroot/dotfiles/vscode/settings.json $dir
+# ln -sf ~/gitroot/dotfiles/vscode/.json $dir
+
 # dir="${HOME}/Library/Application Support/Sublime Text 3/Packages/User"; [ ! -e "${dir}" ] && mkdir -p "${dir}"
 # ln -sf "${HOME}/gitroot/dotfiles/sublime/Default (OSX).sublime-keymap" "${HOME}/Library/Application Support/Sublime Text 3/Packages/User/"
 # ln -sf "${HOME}/gitroot/dotfiles/sublime/Package Control.sublime-settings" "${HOME}/Library/Application Support/Sublime Text 3/Packages/User/"
